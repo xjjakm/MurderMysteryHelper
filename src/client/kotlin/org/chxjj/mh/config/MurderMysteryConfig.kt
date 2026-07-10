c7e1f5052767c5c6449a022631551d70dcf4941a
xjj <xjjakm@outlook.com>
2026-07-10 23:09:17 +0800

    Additional note-Color settings for the configuration menu

--- src/client/kotlin/org/chxjj/mh/config/MurderMysteryConfig.kt
+++ src/client/kotlin/org/chxjj/mh/config/MurderMysteryConfig.kt
@@ -45,9 +45,10 @@ object MurderMysteryConfig {
             .build()
     }
 
-    private fun floatOption(nameKey: String, default: Float, getter: () -> Float, setter: (Float) -> Unit): Option<Float> {
+    private fun floatOption(nameKey: String, descKey: String, default: Float, getter: () -> Float, setter: (Float) -> Unit): Option<Float> {
         return Option.createBuilder<Float>()
             .name(Component.translatable(nameKey))
+            .description(OptionDescription.of(Component.translatable(descKey)))
             .binding(default, getter, setter)
             .controller { option: Option<Float> -> FloatFieldControllerBuilder.create(option).range(0f, 255f) }
             .build()
@@ -80,12 +81,12 @@ object MurderMysteryConfig {
 
         val colorCategory = ConfigCategory.createBuilder()
             .name(Component.translatable("mh.config.category.colors"))
-            .option(floatOption("mh.config.colors.murdererRed", 203f, { murdererRed }, { murdererRed = it }))
-            .option(floatOption("mh.config.colors.murdererGreen", 9f, { murdererGreen }, { murdererGreen = it }))
-            .option(floatOption("mh.config.colors.murdererBlue", 9f, { murdererBlue }, { murdererBlue = it }))
-            .option(floatOption("mh.config.colors.detectiveRed", 0f, { detectiveRed }, { detectiveRed = it }))
-            .option(floatOption("mh.config.colors.detectiveGreen", 144f, { detectiveGreen }, { detectiveGreen = it }))
-            .option(floatOption("mh.config.colors.detectiveBlue", 255f, { detectiveBlue }, { detectiveBlue = it }))
+            .option(floatOption("mh.config.colors.murdererRed", "mh.config.colors.murdererRed.desc", 203f, { murdererRed }, { murdererRed = it }))
+            .option(floatOption("mh.config.colors.murdererGreen", "mh.config.colors.murdererGreen.desc", 9f, { murdererGreen }, { murdererGreen = it }))
+            .option(floatOption("mh.config.colors.murdererBlue", "mh.config.colors.murdererBlue.desc", 9f, { murdererBlue }, { murdererBlue = it }))
+            .option(floatOption("mh.config.colors.detectiveRed", "mh.config.colors.detectiveRed.desc", 0f, { detectiveRed }, { detectiveRed = it }))
+            .option(floatOption("mh.config.colors.detectiveGreen", "mh.config.colors.detectiveGreen.desc", 144f, { detectiveGreen }, { detectiveGreen = it }))
+            .option(floatOption("mh.config.colors.detectiveBlue", "mh.config.colors.detectiveBlue.desc", 255f, { detectiveBlue }, { detectiveBlue = it }))
             .build()
 
         val notifyCategory = ConfigCategory.createBuilder()
