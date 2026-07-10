c96f6dae970769aa526c569d37fcd95e9923681a
xjj <xjjakm@outlook.com>
2026-07-10 18:34:20 +0800

    Initial commit

---
+++ src/client/kotlin/org/chxjj/client/murdermysteryhelperClient.kt
@@ -0,0 +1,9 @@
+package org.chxjj.client
+
+import net.fabricmc.api.ClientModInitializer
+
+class murdermysteryhelperClient : ClientModInitializer {
+
+    override fun onInitializeClient() {
+    }
+}
---
+++ src/main/kotlin/org/chxjj/murdermysteryhelper.kt
@@ -0,0 +1,9 @@
+package org.chxjj
+
+import net.fabricmc.api.ModInitializer
+
+class murdermysteryhelper : ModInitializer {
+
+    override fun onInitialize() {
+    }
+}
